34bcff9abd3053e54d3c1debd53e438bb2c5280c
LeDoc <ledoclh76fr@gmail.com>
2022-09-21 15:28:45 +0200

    Files create environnement.yaml and requirements.txt.

--- make_category_directory.py
+++ make_category_directory.py
@@ -2,11 +2,14 @@ import os
 
 
 def make_category_directory(category_name: str) -> None:
-    """For a category name in attr. make a directory in pictures directory with the formated category_name"""
+    """For a category name in attr. make a directory in pictures directory with the formated category_name
+
+    """
+
     category_name = category_name.replace(" ", "_")
     try:
         os.mkdir("pictures/" + category_name)
-        print("Création")
+        # print("Création")
     except FileExistsError:
         # print("Le repertoire existe déja")
         pass
