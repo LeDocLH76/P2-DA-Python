884d652330afa0d7994dc93e976eb2411c3cccc6
LeDoc <ledoclh76fr@gmail.com>
2022-09-26 13:08:30 +0200

    Rename and reorganize the columns titles in save_category_csv.py and prices order in find_all_books_infos.py

--- save_category_csv.py
+++ save_category_csv.py
@@ -9,16 +9,16 @@ def save_category_csv(category_name, category_book_info):
 
     print(f"Ecriture du fichier pour la categorie : {category_name}")
     column_title = [(
-        "Product_page_url",
-        "Universal_product_code",
-        "Title",
-        "Price_excl_tax £",
-        "Price_incl_Tax £",
-        "Number_avalaible",
-        "Product_description",
-        "Category",
-        "Review_rating /5",
-        "Image.url"
+        "product_page_url",
+        "universal_product_code",
+        "title",
+        "price_including_tax",
+        "price_excluding_tax",
+        "number_avalaible",
+        "product_description",
+        "category",
+        "review_rating",
+        "image_url"
     )]
     column_title.extend(category_book_info)
     with open("csv_files/" + category_name + ".csv", "w", encoding="utf-8", newline='') as new_file:
