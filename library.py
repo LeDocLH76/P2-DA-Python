753b5c2df65844e241f4cf52ebb7e3b80b7ee67f
LeDoc <ledoclh76fr@gmail.com>
2022-09-30 16:21:42 +0200

    instantiation Category OK. Object acces by categories_name[index]

--- library.py
+++ library.py
@@ -1,25 +1,34 @@
 from typing import List
 
+from bs4 import ResultSet
+from constant import BASEURL
+
 from request_api import fetch_page
 from make_the_soup import make_the_soup
 
 
 class Library:
     def __init__(self, base_url) -> None:
-        self.base_url = base_url
         self.site_url = f"{base_url}index.html"
         # print(f"Library init, site url = {self.site_url}")
-        self.categories, self.categories_names = Library.find_categories_links(
-            self)
 
-    def find_categories_links(self) -> List[str]:
-        page_main = fetch_page(self.site_url)
-        page_main_soup = make_the_soup(page_main.content, "html.parser")
-        li_list = page_main_soup.aside.ul.ul.find_all("li")
+    @property
+    def categories_links(self) -> List[str]:
+        li_list = Library.find_li_list(self)
         categories_links = [
-            self.base_url + li.select_one('a')['href'] for li in li_list]
+            BASEURL + li.select_one('a')['href'] for li in li_list]
+        return categories_links
+
+    @property
+    def categories_names(self) -> List[str]:
+        li_list = Library.find_li_list(self)
         categories_names = [
             li.select_one('a').text.strip().replace(" ", "_").lower()
             for li in li_list]
+        return categories_names
 
-        return categories_links, categories_names
+    def find_li_list(self) -> ResultSet:
+        page_main = fetch_page(self.site_url)
+        page_main_soup = make_the_soup(page_main.content, "html.parser")
+        li_list = page_main_soup.aside.ul.ul.find_all("li")
+        return li_list
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from constant import BASEURL
 from library import Library
-# from category import Category
+from category import Category
 
 
 library = Library(BASEURL)
-# print(library.categories)
+# print(library.categories_links)
 # print(library.categories_names)
-for (link, name) in zip(library.categories, library.categories_names):
-    print(f"La catégorie {name} est à l'adresse {link}")
-# 	Category(url)
-# category.name = Category(category)
-# print(f"L'Url du site est: {library.site_url}")
-# print(test_category.books_url)
+# for (link, name) in zip(library.categories_links, library.categories_names):
+#     print(f"La catégorie {name} est à l'adresse {link}")
+# print(library.categories_names[0])
+# print(library.categories_links[0])
+categories_names = library.categories_names
+
+for index in range(len(categories_names)):
+    categories_names[index] = Category(
+        library.categories_links[index])
+    print(categories_names[index].name + " " +
+          categories_names[index].category_url)
+
+    # print(categories_names[index].books_url)
